476004559214ed0ab24f6fba536b3989b688b3da
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-20 15:09:46 +0500

    fix: fix preflight req in CORS middleware

--- app.js
+++ app.js
@@ -28,12 +28,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  next();
+  return next();
 });
 
 app.use('/users', require('./routes/users'));
