bd3cf2daf3fcc5d30928ee65af13e0287bcc9457
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-20 19:32:50 +0500

    fix: fix cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,30 @@ const {
   UnauthorizedError, // 401
 } = require('../errors/errors');
 
+// module.exports = (req, res, next) => {
+//   if (!req.cookies.jwt) {
+//     throw new UnauthorizedError('Необходима авторизация');
+//   } else {
+//     const token = req.cookies.jwt;
+
+//     let payload;
+
+//     try {
+//       payload = jwt.verify(token, 'super-duper-secret-key');
+//     } catch (err) {
+//       throw new UnauthorizedError('Ошибка верификации токена');
+//     }
+
+//     req.user = payload;
+//     next();
+//   }
+// };
+
 module.exports = (req, res, next) => {
-  console.log('TRY TO AUTH');
-  if (!req.cookies.jwt) {
+  if (!req.cookies) {
     throw new UnauthorizedError('Необходима авторизация');
   } else {
-    const token = req.cookies.jwt;
+    const { token } = req.cookies;
 
     let payload;
 
