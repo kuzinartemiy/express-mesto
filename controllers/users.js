38439d24ca32ca1b6efc12f18af0ac27b78f8ad7
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-21 00:35:13 +0500

    fix: fix cookies

--- controllers/users.js
+++ controllers/users.js
@@ -152,7 +152,7 @@ module.exports.login = (req, res, next) => {
                 httpOnly: true,
                 sameSite: true,
                 maxAge: 3600000 * 24,
-              }).send({ message: 'Cookies accepted' });
+              }).send({ token });
             }
           })
 
