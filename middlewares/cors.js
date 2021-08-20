85b724a257755afb07048d2ad380d6407c1a5a07
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-20 14:48:31 +0500

    fix: fix localhost url in CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'https://kuzinartemiy.nomoredomains.monster',
   'http://kuzinartemiy.nomoredomains.monster',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 module.exports = {
