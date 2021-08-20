b39b906608896a3a06dc22d108517ff3bde47201
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-20 15:15:52 +0500

    fix: fix preflight req in CORS middleware

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   }
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   return next();
