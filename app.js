08d85cd7f7da91003bfb0018d4957a0e2b8eae9d
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-23 15:59:24 +0500

    fix: fix login

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
 }), login);
