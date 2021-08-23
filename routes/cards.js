22d71dc632344361a0e2be773f6fc128896f4303
kuzinartemiy <kuzin_artemiy@list.ru>
2021-08-23 10:08:26 +0500

    fix: fix cookies

--- routes/cards.js
+++ routes/cards.js
@@ -30,19 +30,19 @@ router.post('/', celebrate({
   }),
 }), createCard);
 
-router.delete('/:cardId', auth, celebrate({
+router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required(),
   }),
 }), deleteCard);
 
-router.put('/:cardId/likes', auth, celebrate({
+router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required(),
   }),
 }), likeCard);
 
-router.delete('/:cardId/likes', auth, celebrate({
+router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).required(),
   }),
--- routes/users.js
+++ routes/users.js
@@ -16,24 +16,24 @@ const {
   getAuthorizedUser,
 } = require('../controllers/users');
 
-router.get('/', auth, getUsers);
+router.get('/', getUsers);
 
-router.get('/me', auth, getAuthorizedUser);
+router.get('/me', getAuthorizedUser);
 
-router.get('/:userId', auth, celebrate({
+router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).required(),
   }),
 }), getUserById);
 
-router.patch('/me', auth, celebrate({
+router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
-router.patch('/me/avatar', auth, celebrate({
+router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().custom((link) => {
       if (validator.isURL(link, { require_protocol: true })) {
